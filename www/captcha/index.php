3b12ccbc3ddd2c0821156a0c6f22a9cb96346709
drpresq <drpresq@gmail.com>
2019-12-02 11:21:16 -0700

    Adjusted Main Page header bar text.

--- www/captcha/index.php
+++ www/captcha/index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>LAMP STACK</title>
+        <title>Capture the CAPTCHA!</title>
         <link rel="stylesheet" href="/assets/css/bulma.min.css">
     </head>
     <body>
